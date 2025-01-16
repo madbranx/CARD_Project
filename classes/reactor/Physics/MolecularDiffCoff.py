6451b0223acba9e697b4c5bdc9ae7b23fa0a2721
Till <till.kasselmann@gmail.com>
2025-01-16 16:02:49 +0100

    Correct Fuller Equation from VDI, Validated with example from VDI

--- classes/reactor/Physics/MolecularDiffCoff.py
+++ classes/reactor/Physics/MolecularDiffCoff.py
@@ -7,11 +7,16 @@ class MolecularDiffCoff:
     def __init__(self):
         pass
 
-    def calc_fuller(self, T, p,  molar_mass_i, molar_mass_j, diff_volume_i, diff_volume_j):
-        # T in K
-        # p in Pa
-        DiffCoff = (1.013e-7 * T**1.75 * ((molar_mass_i * 1e3)^-1 + (molar_mass_j * 1e3)^-1)**(-0.5) /
-                    ( p * 10e-5 * (diff_volume_i**(1/3) + diff_volume_j**(1/3))))
+    def calc_fuller_diffcoff(self, T, p,  molar_mass_i, molar_mass_j, diff_volume_i, diff_volume_j):
+        # Conversion from SI units to equation units
+        p = p * 1e-5                            # Pa -> bar
+        molar_mass_i = molar_mass_i * 1e2      # kg/mol -> g/mol
+        molar_mass_j = molar_mass_j * 1e2      # kg/mol -> g/mol
+
+        # Diffusion coefficient of i in j after Fuller in m^2/s
+        DiffCoff = ( 1e-4 * (0.00143 * T**1.75 * ((1/molar_mass_i) + (1/molar_mass_j))**(0.5)) /
+                     (p * (2)**(0.5) * (diff_volume_i**(1/3) + diff_volume_j**(1/3))**2))
+
         return DiffCoff
 
     

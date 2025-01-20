7569055dd7bb28b463b3f2f49a13980bfb573f8b
Till <till.kasselmann@gmail.com>
2025-01-20 15:50:30 +0100

    Added GeneralConversion.py class

---
+++ classes/ReactorSpecificQuantities/Parameter/GeneralConversion.py
@@ -0,0 +1,10 @@
+class GeneralConversion:
+    def __init__(self):
+        self.R = 8.314
+
+    def calc_c_fl (self):
+        c_fl = p / (R*T)
+        return c_fl
+
+    def calc_mean_molar_mass (self):
+

4dc5c718f7aac3fceb74a17509713c8c6538e9c4
Till <till.kasselmann@gmail.com>
2025-02-14 15:46:37 +0100

    Added relative discretization

--- classes/Parameters/Discretization.py
+++ classes/Parameters/Discretization.py
@@ -3,6 +3,7 @@ import numpy as np
 class Discretization:
     EQUIDISTANT = 1
     ARRAY = 2
+    RELATIVE_ARRAY = 3
 
     def __init__(self, num_volumes, kind=EQUIDISTANT, **kwargs):
         self.num_volumes = num_volumes
@@ -21,6 +22,8 @@ class Discretization:
             self.faces = self.__equidistant()
         elif self.kind == self.ARRAY:
             self.faces = self.__array()
+        elif self.kind == self.RELATIVE_ARRAY:
+            self.faces = self.__relative_array()            #TODO
         else:
             self.faces = self.__equidistant()
 
@@ -50,6 +53,30 @@ class Discretization:
             faces.extend(segment[:-1])
 
         faces.append(ranges[-1][1])  # Ensure the last end is included
+        return np.array(faces)
+
+    def __relative_array(self):
+        ranges = self.kwargs.get('ranges', [])  # Expect [(start, end, step), ...]
+
+        total_length = self.end - self.start
+        total_weight = sum((factor * (end - start) / total_length) for start, end, factor in ranges)
+
+        faces = []
+        remaining_volumes = self.num_volumes
+
+        for i, (start, end, factor) in enumerate(ranges):
+            segment_length = end - start
+            segment_weight = (factor * segment_length) / total_length
+            n_volumes_range = max(1, int(round((segment_weight / total_weight) * self.num_volumes)))
+            remaining_volumes -= n_volumes_range
+
+            if i == len(ranges) - 1:  # Ensure last segment gets any remaining volumes
+                n_volumes_range += remaining_volumes
+
+            segment = np.linspace(start, end, n_volumes_range + 1)
+            faces.extend(segment[:-1])
+
+        faces.append(ranges[-1][1])
         return np.array(faces)
 
 

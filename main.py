60077c786e915e7d959124300cec7bdd62432a8a
Max Brand <max@brand-mails.de>
2025-02-15 02:47:12 +0100

    updated main: added functions and 1D arcs

--- main.py
+++ main.py
@@ -1,130 +1,184 @@
-from pandas.io.stata import precision_loss_doc
-
 from classes.Postprocessing.Postprocessor import Postprocessor
 from classes.FixedBedReactor import FixedBedReactor
 from classes.Integrator import Integrator
 import numpy as np
 
-# DISCRETIZATION SETTINGS #TODO (TBD: DISCRETIZATION STUDY)
+### SCROLL DOWN FOR MAIN FUNCTION CALLS ###
 
-n_axial = 30
-n_radial = 12
-time_end = 2000
-# ACHTUNG FUMMELFAKTOR:
-time_steps = int(time_end*int((n_axial*n_radial)/100))
-precision = 1e-10 # aktuell direkt implementiert in Integrator, Wert hier keinen Einfluss
+## SIMULATION FUNCTIONS:
+###########################################################################################
+def validation_1D(n_axial, time_end, time_steps):
+    ## 1D CASE WITH VALIDATION PLOT
 
-## 1D CASE WITH VALIDATION PLOT
-
-# reactor = FixedBedReactor(1, n_axial)
-# reactor.setup()
-# integrator = Integrator(reactor)
-# integrator.setup(precision, precision, 0, time_end, time_steps)
-# results = integrator.integrate()
-# postprocessor = Postprocessor(reactor, "../results/01")
-# postprocessor.plot_1D_vs_ValidationData("test", results, time_steps)
-
-
-## 2D CASE WITH RESULT PLOTS FOR T, P, U AND CONVERSION X_CO2
-## WITH VALIDATION OF TEMPERATURE at outermost and innermost cell
-
-# reactor = FixedBedReactor(2, n_axial, n_radial)
-# reactor.setup()
-# integrator = Integrator(reactor)
-# integrator.setup(precision, precision, 0, time_end, time_steps)
-# results = integrator.integrate()
-#
-# postprocessor = Postprocessor(reactor, "../results/02")
-# plot_times = [1, 5, 10, 50, 100] # in %
-# for plot_time in plot_times:
-#     postprocessor.plot2D_Temperature("test2", results, int(plot_time/100*time_steps))
-#
-# postprocessor.plot_Twall_vs_validation("test2", results, time_steps)
-
-
-## 2D Case with 1 radial Element, Plot together with 1D and Validation Date
-
-# # 2D with 1 axial element
-# reactor_2D = FixedBedReactor(2, 100, 1)
-# reactor_2D.setup()
-# integrator = Integrator(reactor_2D)
-# integrator.setup(precision, precision, 0, time_end, time_steps)
-# results_2D = integrator.integrate()
-#
-# # 1D
-# reactor_1D = FixedBedReactor(1, 100)
-# reactor_1D.setup()
-# integrator = Integrator(reactor_1D)
-# integrator.setup(precision, precision, 0, time_end, time_steps)
-# results_1D = integrator.integrate()
-#
-# # Plotting
-# postprocessor = Postprocessor(reactor_2D, "../results/02")
-# postprocessor.plot1D_vsPseudo1D_vs_val("test2", results_1D, results_2D, time_steps)
-#
-
-
-## 2D CASE WITH EXTINCTION AND IGNITION ARCS PLOTS
-#TODO ARCs for 1D
-
-# setting up reactor and integrator
-n_axial = 30
-n_radial = 12
-precision = 1
-reactor = FixedBedReactor(2, n_axial, n_radial)
-reactor.T_wall = 550 # Temperature for ignited reactor
-reactor.setup()
-integrator = Integrator(reactor)
-
-time_end_ref = 3000
-time_steps_ref = int(time_end_ref * int((n_axial * n_radial) / 100))
-time_end_ign = 3000
-time_steps_ign = int(time_end_ign * int((n_axial * n_radial) / 100))
-time_end_ext = 3000
-time_steps_ext = int(time_end_ext * int((n_axial * n_radial) / 100))
-
-# get results of ignited reactor
-integrator.setup(precision, precision, 0, time_end_ref, time_steps_ref)
-result_ignited = integrator.integrate()
-postprocessor = Postprocessor(reactor, "../results/02")
-postprocessor.plot2D_Temperature("test2", result_ignited, time_steps_ref)
-w_i, T, p, u = result_ignited.get_rawValues()
-
-# calculating arcs
-results_ignition = []
-results_extinction = []
-
-#T_walls_ign = np.linspace(350, 550, 4)
-T_walls_ign = [300, 480, 550]
-#T_walls_ign = [300, 340, 360, 380, 400, 420, 440, 460, 480, 500, 525, 550, 600, 650]
-T_walls_ext = T_walls_ign
-for T_wall in T_walls_ign:
-    # calculating ignition arcs
-    print("T_wall = ", T_wall)
-    reactor.T_wall = T_wall
+    reactor = FixedBedReactor(1, n_axial)
+    reactor.T_wall = 550
     reactor.setup()
-    integrator.setup(precision, precision, 0, time_end_ign, time_steps_ref)
-    result_ignition = integrator.integrate()
-    results_ignition.append(result_ignition)
-    postprocessor.plot2D_Temperature("test2", result_ignition, time_steps_ign)
-    #extinction arcs
-    integrator.setup(precision, precision, 0, time_end_ext, time_steps_ref)
-    integrator.set_specific_InitialValues(w_i, T, p, u)
-    result_extinction = integrator.integrate()
-    results_extinction.append(result_extinction)
-    postprocessor.plot2D_Temperature("test2", result_extinction, time_steps_ext)
 
-# T_walls_ext = np.linspace(250, 550, 5)
-# #calculating ignition arcs
-# for T_wall in T_walls_ext:
-#     reactor.T_wall = T_wall
-#     reactor.setup()
+    integrator = Integrator(reactor)
+    integrator.setup(0, time_end, time_steps)
+    results = integrator.integrate()
 
-postprocessor.plot_ignitionArc(results_ignition, results_extinction, T_walls_ign, T_walls_ext, time_steps_ign, time_steps_ext)
+    postprocessor = Postprocessor(reactor, "../results/1D_val")
+    postprocessor.plot_1D_vs_ValidationData("test", results, time_steps)
 
 
+def plot_2D_with_TwallVal(n_axial, n_radial, time_end, time_steps):
+    ## 2D CASE WITH RESULT PLOTS FOR T, P, U AND CONVERSION X_CO2
+    ## WITH VALIDATION OF TEMPERATURE at outermost and innermost cell
 
-# # TESTING FUNCTION
+    reactor = FixedBedReactor(2, n_axial, n_radial)
+    reactor.setup()
+    integrator = Integrator(reactor)
+    integrator.setup(0, time_end, time_steps)
+    results = integrator.integrate()
+
+    postprocessor = Postprocessor(reactor, "../results/2D_withTwallVal")
+    plot_times = [1, 5, 10, 50, 100] # in %
+    for plot_time in plot_times:
+        postprocessor.plot2D_Temperature("test2", results, int(plot_time/100*time_steps))
+
+    postprocessor.plot_Twall_vs_validation("test2", results, time_steps)
+
+
+def pseudo_2D_vs_1D(n_axial, n_radial, time_end, time_steps):
+    # 2D with 1 axial element
+    reactor_2D = FixedBedReactor(2, n_axial, n_radial)
+    reactor_2D.setup()
+    integrator = Integrator(reactor_2D)
+    integrator.setup(0, time_end, time_steps)
+    results_2D = integrator.integrate()
+
+    # 1D
+    reactor_1D = FixedBedReactor(1, n_axial)
+    reactor_1D.setup()
+    integrator = Integrator(reactor_1D)
+    integrator.setup(0, time_end, time_steps)
+    results_1D = integrator.integrate()
+
+    # Plotting
+    postprocessor = Postprocessor(reactor_2D, "../results/p2D_vs_1D")
+    postprocessor.plot1D_vsPseudo1D_vs_val("test2", results_1D, results_2D, time_steps)
+
+def arcs_1d(n_axial, time_end, time_steps, ignited_T, T_walls):
+    ## 1D CASE WITH EXTINCTION AND IGNITION ARCS PLOTS
+
+    #setting up reactor and integrator
+    reactor = FixedBedReactor(1, n_axial)
+    reactor.T_wall = ignited_T # Temperature for ignited reactor
+    reactor.setup()
+    integrator = Integrator(reactor)
+
+    # get results of ignited reactor
+    integrator.setup(0, time_end, time_steps)
+    result_ignited = integrator.integrate()
+    postprocessor = Postprocessor(reactor, "../results/arcs_1d")
+    w_i, T, p, u = result_ignited.get_rawValues()
+
+    # calculating arcs
+    results_ignition = []
+    results_extinction = []
+    for T_wall in T_walls:
+
+        # calculating ignition arcs
+        print("T_wall = ", T_wall)
+        reactor.T_wall = T_wall
+        reactor.setup()
+        try:
+            integrator.setup(0, time_end, time_steps)
+            result_ignition = integrator.integrate()
+            results_ignition.append(result_ignition)
+        except:
+            pass
+
+        #extinction arcs
+        try:
+            integrator.set_specific_InitialValues(w_i, T, p, u)
+            result_extinction = integrator.integrate()
+            results_extinction.append(result_extinction)
+        except:
+            pass
+
+    postprocessor.plot_ignitionArc1D(results_ignition, results_extinction, T_walls, time_steps)
+
+
+def arcs_2d(n_axial, n_radial, time_end, time_steps, ignited_T, T_walls, plotting=False):
+    ## 2D CASE WITH EXTINCTION AND IGNITION ARCS PLOTS
+
+    # setting up reactor and integrator
+    reactor = FixedBedReactor(2, n_axial, n_radial)
+    reactor.T_wall = ignited_T # Temperature for ignited reactor
+    reactor.setup()
+    integrator = Integrator(reactor)
+
+    # get results of ignited reactor
+    integrator.setup(0, time_end, time_steps)
+    result_ignited = integrator.integrate()
+    postprocessor = Postprocessor(reactor, "../results/arcs_2d")
+    w_i, T, p, u = result_ignited.get_rawValues()
+
+    # calculating arcs
+    results_ignition = []
+    results_extinction = []
+    for T_wall in T_walls:
+
+        # calculating ignition arcs
+        print("T_wall = ", T_wall)
+        reactor.T_wall = T_wall
+        reactor.setup()
+        try:
+            integrator.setup(0, time_end, time_steps)
+            result_ignition = integrator.integrate()
+            results_ignition.append(result_ignition)
+            if plotting:
+                postprocessor.plot2D_Temperature("test2", result_ignition, time_steps)
+        except:
+            pass
+
+        #extinction arcs
+        try:
+            integrator.setup(0, time_end, time_steps)
+            integrator.set_specific_InitialValues(w_i, T, p, u)
+            result_extinction = integrator.integrate()
+            results_extinction.append(result_extinction)
+            if plotting:
+                postprocessor.plot2D_Temperature("test2", result_extinction, time_steps)
+        except:
+            pass
+
+    postprocessor.plot_ignitionArc(results_ignition, results_extinction, T_walls, time_steps)
+
+
+###########################################################################################
+
+#validation_1D(40, 3000, 200)
+
+#arcs_1d(60, 4000, 500, 550, np.linspace(350, 500, 5))
+
+plot_2D_with_TwallVal(150, 12, 3000, 30000)
+
+#pseudo_2D_vs_1D(20, 5, 3000, 300)
+
+#arcs_2d(20, 5, 3000, 300, 550, [400, 450, 500])
+
+###########################################################################################
+
+#TODO
+# Fix numerical issues & radial dispersion
+# .
+# .
+# Create ULM diagram             TBD
+# add Diskretisierungsformeln in PDF
+# .
+# Postprocessing (2D)            WORK IN PROGRESS
+#       (TBD: DISCRETIZATION STUDY)
+# .     Extinction / Ignition ARC Plots
+# .     Effectiveness etc. Plot
+# .     RMSE/MAPE for one Value
+
+###########################################################################################
+
+## TESTING FUNCTIONS
+
 # import casadi as CasADi
 # T = 500
 # p = 5e5
@@ -146,16 +200,4 @@ postprocessor.plot_ignitionArc(results_ignition, results_extinction, T_walls_ign
 # print(reactor.calc_eff_disp_coeff(wTpu1, 3))
 
 
-
-#TODO
-# Fix numerical issues & radial dispersion
-# .
-# .
-# Create ULM diagram             TBD
-# add Diskretisierungsformeln in PDF
-# .
-# Postprocessing (2D)            WORK IN PROGRESS
-# .     Extinction / Ignition ARC Plots
-# .     Effectiveness etc. Plot
-# .     RMSE/MAPE for one Value
 

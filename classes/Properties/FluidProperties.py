1a44d9545cd44c1ca93c5fb2dfecee439beef628
Till <till.kasselmann@gmail.com>
2025-02-20 15:27:17 +0100

    Added Comments

--- classes/Properties/FluidProperties.py
+++ classes/Properties/FluidProperties.py
@@ -3,6 +3,12 @@ from classes.Parameters.Component import Component
 
 import casadi as CasADi
 
+"""
+The FluidProperties class is used to define methodes for calculation of parameters of the fluid phase.
+This includes dimensionsionless quantitys (Re, Pr, Pe), the thermal conductivity with viscosity mixing rule and general
+properties as desnity and mass fraction weighted averages.
+"""
+
 
 class FluidProperties(Parameters):
 
@@ -10,15 +16,17 @@ class FluidProperties(Parameters):
         super().__init__()
 
     def rho_comp(self, w_i, T, p):
+        # Calculate density of components
         return self.concentrations(w_i, T, p)*self.getMolarWeights()
 
     def rho_fl(self, w_i, T, p):
+        # Calculate density of fluid phase
         Mw_fl = self.massFraction_weighted_average(w_i, Component.MOLECULAR_WEIGHT)
         rho = self.__c_fl(T, p) * Mw_fl
         return rho
 
-    # mean fluid concentration
     def __c_fl(self, T, p):
+        # Calculate mean fluid concentration
         cf = p / (self.R * T)
         return cf
 
@@ -28,8 +36,9 @@ class FluidProperties(Parameters):
             Mw_i.append(component.get_property(Component.MOLECULAR_WEIGHT))
         return Mw_i
 
-    # mass fraction weighted average of a fluid property
-    def massFraction_weighted_average(self, w_i, material_property, T=None):  # property = Component.DENSITY, ...
+    def massFraction_weighted_average(self, w_i, material_property, T=None):
+        # Methode to calculate mass fraction weighted averages of fluid properties
+        # property = Component.DENSITY, ...
         property_values = []
         for component in self.components:
             property_values.append(component.get_property(material_property, T))
@@ -46,6 +55,7 @@ class FluidProperties(Parameters):
 
 
     def moleFractions(self, w_i):
+        # Methode to convert mass fractions into mole fractions
         Mw_i = self.getMolarWeights()
         moles = w_i / Mw_i
         tot_moles = CasADi.sum1(moles)
@@ -53,13 +63,17 @@ class FluidProperties(Parameters):
         return mole_fractions
 
     def partial_pressures(self, w_i, p):
+        # Methode to convert mass fractions into partial pressures
         return CasADi.SX(self.moleFractions(w_i)*p)
 
     def concentrations(self, w_i, T, p):
+        # Methode to convert mass fractions into concentrations
         p_i = self.partial_pressures(w_i, p)
         return p_i/(self.R*T)
 
     def rho_cp_eff(self,w_i, T, p):
+        # Methode to calculate the effective heat capacity of the cat + fluid phase
+        # Eq. (32)
         cp_fl = self.massFraction_weighted_average(w_i, Component.HEAT_CAPACITY, T)
         rho_fl = self.rho_fl(w_i, T, p)
 
@@ -70,6 +84,8 @@ class FluidProperties(Parameters):
         return rho_cp_eff
 
     def Re(self, w_i, T, u, p):
+        # Methode for Reynolds number
+        # Eq. (37)
         eta_fl = self.massFraction_weighted_average(w_i, Component.DYNAMIC_VISCOSITY, T)
         d_p = self.cat_diameter
         rho_fl = self.rho_fl(w_i, T, p)
@@ -78,7 +94,8 @@ class FluidProperties(Parameters):
         return Re
 
     def Re_0 (self, T, p, u, w_i):
-        # Reynolds number with velocity corrected for emtpy space velocity
+        # Methode for Reynolds number with velocity corrected for emtpy space velocity
+        # Eq. (43)
         eps = self.eps
         eta_fl = self.massFraction_weighted_average(w_i, Component.DYNAMIC_VISCOSITY, T)
         d_p = self.cat_diameter
@@ -90,8 +107,8 @@ class FluidProperties(Parameters):
         return Re_0
 
     def Pe_0 (self, T, p, u, w_i):
-        # Peclet number with velocity corrected for empty space velocity
-        # Thermal Conductivity
+        # Methode for Peclet number with velocity corrected for empty space velocity for thermal conductivity
+        # Eq. (40)
         eps = self.eps
         cp_fl = self.massFraction_weighted_average(w_i, Component.HEAT_CAPACITY, T)
         lambda_fl = self.calc_fluidThermalConductivity(T, w_i)
@@ -104,8 +121,8 @@ class FluidProperties(Parameters):
         return Pe_0
 
     def Pe_0_diff(self, u, diffCoff):
-        # Peclet number with velocity corrected for empty space velocity
-        # Diffusion
+        # Methode for Peclet number with velocity corrected for empty space velocity for diffusion
+        # Eq. (17)
         eps = self.eps
         d_p = self.cat_diameter
 
@@ -115,6 +132,8 @@ class FluidProperties(Parameters):
         return Pe_0_diff
 
     def Pr(self, w_i, T):
+        # Methode for Prandtl number
+        # Eq. (38)
         eta_fl = self.massFraction_weighted_average(w_i, Component.DYNAMIC_VISCOSITY, T)
         cp_fl = self.massFraction_weighted_average(w_i, Component.HEAT_CAPACITY, T)
         lambda_fl = self.calc_fluidThermalConductivity(T, w_i)
@@ -123,6 +142,8 @@ class FluidProperties(Parameters):
         return Pr
 
     def calc_fluidThermalConductivity(self, T, w_i):
+        # Methode to calculate the thermal conductivity of the fluid phase with utilization viscosity mixing rule
+        # Eq. (52)
         # Get component properties
         viscosity = []
         thermal_conductivity = []
@@ -144,6 +165,7 @@ class FluidProperties(Parameters):
 
     def __calc_compActivity(self, molar_weight_i, molar_weight_j, viscosity_i, viscosity_j):
         # Component activity for viscous mixing rule utilized in calculating thermal conductivity of a fluid mixture
+        # Eq. (53)
         compActivity = (
                 (1 + CasADi.sqrt(viscosity_i / viscosity_j) * (molar_weight_j / molar_weight_i) ** (1 / 4)) ** 2 /
                 (CasADi.sqrt(8 * (1 + molar_weight_i / molar_weight_j))))
--- classes/Properties/Kinetics.py
+++ classes/Properties/Kinetics.py
@@ -2,7 +2,10 @@ import casadi as CasADi
 
 from classes.Parameters.Component import Component
 from classes.Properties.FluidProperties import FluidProperties
-
+"""
+The class Kinetics conatins the methodes to calculate the rate of the chemical reaction. The rate expression is calculated with
+a LHHW model. Since only the methanisation reaction is simulated, no further rate expressions are required.
+"""
 
 class Kinetics(FluidProperties):
     def __init__(self):
@@ -22,17 +25,21 @@ class Kinetics(FluidProperties):
         self.reactionEnthalpy = -164900
 
     def rate_equation(self, w_i, T, p):
-        # p_i in bar
-        # [p_CH4, p_H2O, p_CO2, p_H2] = self.partial_pressures(w_i, p) * 1e-5
-        p_i = self.partial_pressures(w_i, p) * 1e-5
+        # Methode to calculate the rate expression after a LHHW kinetic model
+
+        p_i = self.partial_pressures(w_i, p) * 1e-5     # Conversion Pa -> bar
         p_CH4 = p_i[0]
         p_H2O = p_i[1]
         p_CO2 = p_i[2]
         p_H2 = p_i[3]
 
+        # Eq. (24)
         k = self.__calc_k(T)
+        # Eq. (26)
         K_eq = self.__K_eq(T)
+        # K_x with eq. (25)
 
+        # Eq. (23)
         r = (k * (p_CO2 ** 0.5) * (p_H2 ** 0.5) * (1 - (p_CH4 * (p_H2O ** 2)) / (K_eq * p_CO2 * (p_H2 ** 4))) /
              (1 + self.__calc_K_x('OH', T) * p_H2O / (p_H2 ** 0.5) + self.__calc_K_x('H2', T) * (
                          p_H2 ** 0.5) + self.__calc_K_x('mix', T) * (p_CO2 ** 0.5))**2)
@@ -42,26 +49,25 @@ class Kinetics(FluidProperties):
 
         # Conversion of r from [mol/(g_cat*s)] into [mol/(m^3_cat*s)]
         r *= rho_cat
-
         return r
 
-    # methode to calculate rate coefficient k
     def __calc_k(self, T):
-        # T in K
+        # Methode to calculate the rate coefficient
+        # Eq. (24)
         k = self.k0_ref * CasADi.exp(self.E_A / self.R * (1 / self.T_ref - 1 / T))
         return k
 
-    # methode to calculate adsorption constant K_x for OH, H2 and mix
     def __calc_K_x(self, species, T):
-        # T in K
+        # Methode to calculate adsorption constant K_x for OH, H2 and mix
+        # Eq. (25)
         K_x_ref = self.AdsorptionConstants[species]['K_ref']
         dH_x = self.AdsorptionConstants[species]['dH']
 
         K_x = K_x_ref * CasADi.exp(dH_x / self.R * (1 / self.T_ref - 1 / T))
         return K_x
 
-    # methode to calculate equilibrium constant K_eq
     def __K_eq(self, T):
-        # T in K
+        # Methode to calculate equilibrium constant K_eq
+        # Eq. (26)
         K_eq = 137 * T ** (-3.998) * CasADi.exp(158.7e3 / (self.R * T))
         return K_eq
